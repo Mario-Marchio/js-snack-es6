2272720c145b91062275bd3a3ec3a4a277dbb6bd
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-13 15:36:20 +0200

    es 1 done

--- script1.js
+++ script1.js
@@ -1,2 +1,12 @@
 // es1
 const invited = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+
+
+const details = invited.map((ospite, i) => {
+    return {
+      tavolo: 'tavolo vip',
+      nome: ospite,
+      posto: i + 1
+    };
+  });
+  console.log(details);
